ab2c6d1428ce28bf190487b93d9db3fbf17ddc6e
Joel Teply <joelteply@yahoo.com>
2015-03-27 15:56:57 -0500

    initial checkin

---
+++ SwiftAugmentedDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftAugmentedDemo
+//
+//  Created by Joel Teply on 3/27/15.
+//  Copyright (c) 2015 Joel Teply. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
